6e20e7f90b794c2fd4dcf070a30abc2e61a443e6
yankazabka <yanka.zabka@gmail.com>
2023-12-12 21:40:19 +0100

    feat: add divider-item component

---
+++ src/components/divider-item/DividerItem.vue
@@ -0,0 +1,46 @@
+<template>
+  <fragment>
+    <div class="divider-wrapper">
+      <hr>
+      <span v-if="text">{{ text }}</span>
+      <hr>
+    </div>
+  </fragment>
+</template>
+
+<style scoped>
+.divider-wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  width: 100%;
+
+  max-width: 568px;
+}
+
+hr {
+  border: 0;
+  flex-grow: 1;
+  background-color: #E9E9E9;
+  height: 1px;
+}
+
+span {
+  font-weight: 300;
+  font-size: 16px;
+  line-height: 24px;
+  color: #413C3C;
+  background-color: white;
+  padding: 0 10px;
+}
+</style>
+<script>
+export default {
+  props: {
+    text: {
+      type: String
+    }
+  }
+}
+</script>
---
+++ src/components/divider-item/index.js
@@ -0,0 +1,1 @@
+export {default} from './DividerItem.vue';
