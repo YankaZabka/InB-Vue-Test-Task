ae91ede52b6870b42fdc04dbc7afd90230ac047a
yankazabka <yanka.zabka@gmail.com>
2023-12-13 01:04:07 +0100

    fix(user-info-edit): layout

--- src/components/user-info/components/edit-info/EditInfo.vue
+++ src/components/user-info/components/edit-info/EditInfo.vue
@@ -1,29 +1,31 @@
 <template>
-  <b-container class="main-container">
-    <b-container class="wrapper p-0">
+  <b-container class="main-container centered-container">
 
       <b-row class="row-wrapper m-0">
-        <b-col cols="6" class="d-flex justify-content-between align-items-center">
-          <div class="centered-container flex-shrink-0">Change your contact Info</div>
 
+        <b-col sm="12" md="4">
+          <div class="centered-container flex-shrink-0">Change your contact Info</div>
+        </b-col>
+
+        <b-col sm="12" md="7" class="centered-container my-sm-2 my-md-0 justify-content-md-start" style="gap: 20px">
           <div class="centered-container" style="gap: 6px">
             <img :src="UserPhoneIcon" alt="UserPhoneIcon" >
             <input class="input-element" v-model="editedInfo.userPhone">
           </div>
-        </b-col>
-        <b-col cols="6" class="d-flex justify-content-between p-0">
           <div class="centered-container" style="gap: 6px">
             <img :src="UserMailIcon" alt="UserMailIcon" >
-            <input class="input-element" v-model="editedInfo.userMail">
+            <input class="input-element" v-model="editedInfo.userMail" style="min-width: 150px">
           </div>
+        </b-col>
 
+        <b-col sm="12" md="1" class="d-flex justify-content-end p-0">
           <div class="icon-button toggle-button" @click="toggleView">
             <img :src="UserViewInfoButton" alt="UserViewInfoButton" >
           </div>
         </b-col>
+
       </b-row>
 
-    </b-container>
   </b-container>
 </template>
 
@@ -52,12 +54,14 @@ export default {
 <style scoped>
 .main-container {
   background-color: #F8F5FC;
-  height: 58px;
+  min-height: 58px;
 
   border-radius: 30px;
   max-width: 800px;
 
   margin-bottom: 10px;
+
+  padding: 12px;
 }
 
 .wrapper {
@@ -75,14 +79,14 @@ export default {
 .input-element {
   color: #413C3C;
   font-weight: 400;
-  font-size: 20px;
+  font-size: 14px;
   line-height: 24px;
 
   background-color: transparent;
   border: none;
   border-bottom: 1px solid #DEDEDE;
 
-  max-width: 100px;
+  max-width: 80px;
 }
 
 .input-element:focus {
